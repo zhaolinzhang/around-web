dfe4f3f8b9d5b69300268e131a8ea9bb301c2e5d
zhaolinzhang <zz1296@nyu.edu>
2018-08-01 16:22:20 +0800

    ant

---
+++ src/Main.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { WrappedRegistrationForm } from './Register';
+
+export class Main extends React.Component {
+    render() {
+        return (
+            <div>
+                <WrappedRegistrationForm/>
+            </div>
+        );
+    }
+}
