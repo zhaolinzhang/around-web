c039481741627b62fba015642c7c13d87ed10908
zhaolinzhang <zz1296@nyu.edu>
2018-08-08 16:24:00 +0800

    image blank finished

---
+++ src/components/Gallery.js
@@ -0,0 +1,61 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import GridGallery from 'react-grid-gallery';
+
+export class Gallery extends React.Component {
+    static propTypes = {
+        images: PropTypes.arrayOf(
+            PropTypes.shape({
+                user: PropTypes.string.isRequired,
+                src: PropTypes.string.isRequired,
+                thumbnail: PropTypes.string.isRequired,
+                caption: PropTypes.string,
+                thumbnailWidth: PropTypes.number.isRequired,
+                thumbnailHeight: PropTypes.number.isRequired
+            })
+        ).isRequired
+    }
+
+    render() {
+        const images = this.props.image.map((image) => {
+            return {
+                ...image,
+                customOverlay: (
+                    <div style={captionStyle}>
+                        <div>{`${image.user}: ${image.caption}`}</div>
+                    </div>
+                )
+            };
+        });
+
+        return (
+            <div style={wrapperStyle}>
+                <GridGallery
+                    images={images}
+                    enableImageSelection={false}
+                    backdropClosesModal={true}
+                />
+            </div>
+        )
+    }
+}
+
+const captionStyle = {
+    backgroundColor: "rgba(0, 0, 0, 0.8)",
+    maxHeight: "240px",
+    overflow: "hidden",
+    position: "absolute",
+    bottom: "0",
+    width: "100%",
+    color: "white",
+    padding: "2px",
+    fontSize: "90%"
+};
+
+const wrapperStyle = {
+    display: "block",
+    minHeight: "1px",
+    width: "100%",
+    border: "1px solid #ddd",
+    overflow: "auto"
+};
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,103 @@
 import React from 'react';
+import $ from 'jquery';
+import { Tabs, Button, Spin } from 'antd';
+import { GEO_OPTIONS, POS_KEY, API_ROOT, TOKEN_KEY, AUTH_PREFIX } from '../constants';
+import { Gallery } from './Gallery';
+
+const TabPane = Tabs.TabPane;
+
+const operations = <Button>Extra Action</Button>;
 
 export class Home extends React.Component {
+
+    state = {
+        loadingGeoLocation: false,
+        loadingPosts: false,
+        error: '',
+        posts: []
+    }
+
+    componentDidMount() {
+        this.setState({ loadingGeoLocation: true, error: '' });
+        this.getGeoLocation();
+    }
+
+    getGeoLocation = () => {
+        if ("geolocation" in navigator) {
+            navigator.geolocation.getCurrentPosition(
+                this.onSuccessLoadGeoLocation,
+                this.onFailedLoadGeoLocation,
+                GEO_OPTIONS
+            );
+        } else {
+            this.setState({ error: "Your browser does not support geolocation!" });
+        }
+    }
+
+    onSuccessLoadGeoLocation = (position) => {
+        console.log(position);
+        const { latitude, longitude } = position.coords;
+        localStorage.setItem(POS_KEY, JSON.stringify({latitude, longitude}));
+        this.setState({ loadingGeoLocation: false, error: ''})
+        this.loadNearbyPosts();
+    }
+
+    onFailedLoadGeoLocation = () => {
+        this.setState({ loadingGeoLocation: false, error: "Failed to get user location" });
+    }
+
+    getGalleryPanelContent = () => {
+        if (this.state.error) {
+            return <div>{this.state.error}</div>
+        } else if (this.state.loadingGeoLocation) {
+            return <Spin tip="Loading Geo location..."/>;
+        } else if (this.state.loadingPosts) {
+            return <Spin tip="Loading posts..."/>;
+        } else if (this.state.posts && this.state.posts.length > 0) {
+            const images = this.state.posts.map(({user, message, url}) => {
+                return {
+                    user,
+                    caption: message,
+                    src: url,
+                    thumbnail: url,
+                    thumbnailWidth: 400,
+                    thumbnailHeight: 300,
+                };
+            });
+            return (
+                <div>
+                    <Gallery images={images}/>
+                </div>
+            );
+        } else {
+            return null;
+        }
+    }
+
+    loadNearbyPosts = () => {
+        const { latitude, longitude } = JSON.parse(localStorage.getItem(POS_KEY));
+        this.setState({ loadingPosts: true });
+        $.ajax({
+            url: `${API_ROOT}/search?lat=${latitude}&lon=${longitude}&range=20000`,
+            method: 'GET',
+            headers: {
+                Authorization: `${AUTH_PREFIX} ${localStorage.getItem(TOKEN_KEY)}`
+            }
+        }).then((posts) => {
+            console.log(posts);
+            this.setState({ loadingPosts: false, error: '', posts });
+        }, (error) => {
+            this.setState({ loadingPosts: false, error: error.responseText });
+        });
+    }
+
     render() {
+        console.log(this.state);
         return (
-            <div>This is home</div>
+            <Tabs tabBarExtraContent={operations} className="main-tabs">
+                <TabPane tab="Posts" key="1">{this.getGalleryPanelContent()}</TabPane>
+                <TabPane tab="Map" key="2">Content of tab 2</TabPane>
+            </Tabs>
         );
     }
 }
